cb0f02bfebbf78af2b4ac686ebc91a8ca12fc01e
Henrique Vicente <henriquevicente@gmail.com>
2024-01-18 08:40:59 +0100

    wip

---
+++ integration/users/connection_test.go
@@ -0,0 +1,133 @@
+package users
+
+import (
+	"context"
+	"testing"
+
+	"github.com/FerretDB/FerretDB/integration/setup"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/topology"
+)
+
+func TestAuthentication(t *testing.T) {
+	t.Parallel()
+
+	s := setup.SetupWithOpts(t, &setup.SetupOpts{})
+	ctx := s.Ctx
+	collection := s.Collection
+	db := collection.Database()
+
+	testCases := map[string]struct { //nolint:vet // for readability
+		username       string
+		password       string
+		updatePassword string // if true, the password will be updated to this one after the user is created.
+		mechanism      string
+
+		wrongPassword bool
+	}{
+		"Success": {
+			username: "common",
+			password: "password",
+		},
+		"Updated": {
+			username:       "updated",
+			password:       "pass123",
+			updatePassword: "somethingelse",
+		},
+		"Scramsha256": {
+			username:  "scramsha256",
+			password:  "password",
+			mechanism: "SCRAM-SHA-256",
+		},
+		"Scramsha256updated": {
+			username:       "scramsha256updated",
+			password:       "pass123",
+			updatePassword: "anotherpassword",
+			mechanism:      "SCRAM-SHA-256",
+		},
+		"BadPassword": {
+			username:      "baduser",
+			password:      "something",
+			mechanism:     "SCRAM-SHA-256",
+			wrongPassword: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			if tc.mechanism == "PLAIN" {
+				setup.SkipForMongoDB(t, "PLAIN credentials are only supported via LDAP (PLAIN) by MongoDB Enterprise")
+			}
+
+			createPayload := bson.D{
+				{"createUser", tc.username},
+				{"roles", bson.A{}},
+				{"pwd", tc.password},
+			}
+
+			switch tc.mechanism {
+			case "": // Use default mechanism for MongoDB and SCRAM-SHA-256 for FerretDB as SHA-1 won't be supported as it's deprecated.
+				if !setup.IsMongoDB(t) {
+					createPayload = append(createPayload, bson.E{"mechanisms", bson.A{"SCRAM-SHA-256"}})
+				}
+			case "SCRAM-SHA-256", "PLAIN":
+				createPayload = append(createPayload, bson.E{"mechanisms", bson.A{tc.mechanism}})
+			default:
+				t.Fatalf("unimplemented mechanism %s", tc.mechanism)
+			}
+
+			err := db.RunCommand(ctx, createPayload).Err()
+			require.NoErrorf(t, err, "cannot create user")
+
+			if tc.updatePassword != "" {
+				updatePayload := bson.D{
+					{"updateUser", tc.username},
+					{"pwd", tc.updatePassword},
+				}
+
+				err = db.RunCommand(ctx, updatePayload).Err()
+				require.NoErrorf(t, err, "cannot update user")
+			}
+
+			serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+
+			password := tc.password
+			if tc.updatePassword != "" {
+				password = tc.updatePassword
+			}
+			if tc.wrongPassword {
+				password = "wrongpassword"
+			}
+
+			credential := options.Credential{
+				AuthMechanism: tc.mechanism,
+				AuthSource:    db.Name(),
+				Username:      tc.username,
+				Password:      password,
+			}
+
+			opts := options.Client().ApplyURI(s.MongoDBURI).SetServerAPIOptions(serverAPI).SetAuth(credential)
+
+			client, err := mongo.Connect(ctx, opts)
+			require.NoError(t, err, "cannot connect to MongoDB")
+
+			// Ping to force connection to be established and tested.
+			err = client.Ping(ctx, nil)
+
+			if tc.wrongPassword {
+				var ce topology.ConnectionError
+				require.ErrorAs(t, err, &ce, "expected a connection error")
+				return
+			}
+
+			require.NoError(t, err, "cannot ping MongoDB")
+			require.NoError(t, client.Disconnect(context.Background()))
+		})
+	}
+}
